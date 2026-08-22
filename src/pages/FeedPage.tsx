eead7132e2f9011f7106cefa30093bad67bae5d2
Clely <clelyfernandes19@gmail.com>
2026-08-22 17:35:24 -0500

    Align group feed list spacing

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -954,7 +954,7 @@ const FeedPage: React.FC = () => {
             ) : contentFilter === 'projects' ? (
               renderProjectsContent()
             ) : isGroupContentFilter(contentFilter) ? (
-              <div className={`space-y-4 ${viewMode === 'scroll' ? 'max-w-2xl mx-auto' : ''}`}>
+              <div className="space-y-4">
                 {(() => {
                   if (groupsLoading) {
                     return (
@@ -1011,7 +1011,7 @@ const FeedPage: React.FC = () => {
                           )}
                         </div>
                       ) : viewMode === 'scroll' ? (
-                        <div className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-4 max-w-2xl mx-auto">
                           {groupFeedItems.map((item) => (
                             <FeedItem
                               key={`group-${item.type}-${item.id}`}
